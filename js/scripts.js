c2b103f9db502ebee03f1e0a965b112c58a88426
IamElaya <asison13@gmail.com>
2016-09-23 23:43:53 +0000

    Form Validations

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,87 @@
 $(function() {
-  $('#inputName').on('blur', function(){
+  $('#inputName').on('focusout', function(){
     checkName($('#inputName').val())
-    // checkCarMake($('#inputCarMake').val())
-    // checkCarModel($('#inputCarModel').val())
-    // checkCarYear($('#inputCarYear').val())
-    // checkAddress($('#inputAddress').val())
+    // checkPhoneNumber($('#inputPhoneNumber').val())
+  });
+
+  $('#inputCarMake').on('focusout', function(){
+    checkCarMake($('#inputCarMake').val())
+    // checkPhoneNumber($('#inputPhoneNumber').val())
+  });
+
+  $('#inputCarModel').on('focusout', function(){
+    checkCarModel($('#inputCarModel').val())
+    // checkPhoneNumber($('#inputPhoneNumber').val())
+  });
+
+  $('#inputCarYear').on('focusout', function(){
+    checkCarYear($('#inputCarYear').val())
+    // checkPhoneNumber($('#inputPhoneNumber').val())
+  });
+
+  $('#inputAddress').on('focusout', function(){
+    checkAddress($('#inputAddress').val())
+    // checkPhoneNumber($('#inputPhoneNumber').val())
+  });
+
+  $('#inputEmail').on('focusout', function(){
+    checkEmail($('#inputEmail').val())
+    // checkPhoneNumber($('#inputPhoneNumber').val())
+  });
+
+  $('#inputPhoneNumber').on('focusout', function(){
+    checkPhoneNumber($('#inputPhoneNumber').val())
     // checkPhoneNumber($('#inputPhoneNumber').val())
   });
 
   function checkName(name) {
     if (name.match(/^[a-z ,.'-]+$/i))
-      console.log("HEY")
+      $("#nameForm p").text("");
     else
-      console.log("NO")
+      $("#nameForm p").text("Please Put a Name").css({color:"Red"});
+      // var needName = $('#inputName').attr("placeholder", "Please put a name");
   }
 
   function checkCarMake(carMake) {
     if (carMake.match(/^[a-z ,.'-]+$/i))
-      console.log("HEY")
+      $("#carMakeForm p").text("");
     else
-      console.log("NO")
+      $("#carMakeForm p").text("Please Put a Car Make").css({color:"Red"});
   }
 
   function checkCarModel(carModel) {
     if (carModel.match(/^[a-z ,.'-]+$/i))
-      console.log("HEY")
+      $("#carModelForm p").text("");
     else
-      console.log("NO")
+      $("#carModelForm p").text("Please Put a Car Model").css({color:"Red"});
   }
 
   function checkCarYear(carYear) {
     if (carYear.match(/^\d{4}$/))
-      console.log("HEY")
+      $("#carYearForm p").text("");
     else
-      console.log("NO")
+      $("#carYearForm p").text("Please Put a Car Year").css({color:"Red"});
   }
 
   function checkAddress(address) {
     if (address)
-      console.log("HEY")
+      $("#addressForm p").text("");
     else
-      console.log("NO")
+      $("#addressForm p").text("Please Put an address").css({color:"Red"});
+  }
+
+  function checkEmail(email) {
+    if (email)
+      $("#emailForm p").text("");
+    else
+      $("#emailForm p").text("Please Put an email").css({color:"Red"});
   }
 
   function checkPhoneNumber(phoneNumber) {
     if (phoneNumber.match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/))
-      console.log("HEY")
+      $("#phoneNumberForm p").text("");
     else
-      console.log("NO")
+      $("#phoneNumberForm p").text("Please Put a phone number").css({color:"Red"});
   }
 });
 
