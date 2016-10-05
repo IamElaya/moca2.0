2687146943ac0fb5a2a98bd52880f659b2cc952f
Elaya Sison <asison13@gmail.com>
2016-10-05 11:28:33 -0700

    Scrolling Added

--- js/scripts.js
+++ js/scripts.js
@@ -121,3 +121,18 @@ $(function() {
   });
   
 });
+
+$(function scroll() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
